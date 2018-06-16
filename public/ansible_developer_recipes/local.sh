b0f3979ffd1f2ace092ab73706c1c0d176792ae9
usertest <ram_kannan@symantec.com>
2018-06-16 20:03:44 +0530

    updating files

--- public/ansible_developer_recipes/local.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-stty_orig=`stty -g`
-stty -echo
-read -p "become pass: " BECOME_PASS
-stty $stty_orig
-ansible-playbook -i "localhost," -c local local.yml --extra-vars "'ansible_become_pass=${BECOME_PASS}'"
